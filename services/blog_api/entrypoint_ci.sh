c7a6f34843dd53f77c2191a4a539513c00f06a47
Alex Czerny <alexczerny1@gmail.com>
2022-10-09 20:03:58 +0200

    ci pipeline

---
+++ services/blog_api/entrypoint_ci.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+if [ "$DATABASE" = "postgresql" ]
+then
+    echo ">>> [${BLUE}INFO${NC}]: WAITING FOR POSTGRESQL DATABASE."
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+        sleep 0.1
+    done
+    echo ">>> [${GREEN}SUCCESS${NC}]: POSTGRESQL DATABASE STARTED."
+fi
+
+cd ./api/
+
+echo ">>> [${BLUE}INFO${NC}]: EXECUTING $@."
+exec "$@"
