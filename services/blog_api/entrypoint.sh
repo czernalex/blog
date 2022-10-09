5adbdc663f0cc72074b96a997701dba50282581f
Alex Czerny <alexczerny1@gmail.com>
2022-10-09 21:20:14 +0200

    modified ci

--- services/blog_api/entrypoint.sh
+++ services/blog_api/entrypoint.sh
@@ -14,50 +14,53 @@ then
     echo ">>> [${GREEN}SUCCESS${NC}]: POSTGRESQL DATABASE STARTED."
 fi
 
-echo ">>> [${BLUE}INFO${NC}]: FLUSH DATABASE."
-python ./api/manage.py flush --noinput
-if [ $? -eq 0 ]
+if [ "$ENVIRONMENT" = "local" ]
 then
-    echo ">>> [${GREEN}SUCCESS${NC}]: DATABASE FLUSHED."
-else
-    echo ">>> [${RED}ERROR${NC}]: FLUSHING DATABASE FAILED."
-fi
+    echo ">>> [${BLUE}INFO${NC}]: FLUSH DATABASE."
+    python ./api/manage.py flush --noinput
+    if [ $? -eq 0 ]
+    then
+        echo ">>> [${GREEN}SUCCESS${NC}]: DATABASE FLUSHED."
+    else
+        echo ">>> [${RED}ERROR${NC}]: FLUSHING DATABASE FAILED."
+    fi
 
-echo ">>> [${BLUE}INFO${NC}]: DJANGO MAKEMIGRATIONS."
-python ./api/manage.py makemigrations
-if [ $? -eq 0 ]
-then
-    echo ">>> [${GREEN}SUCCESS${NC}]: DJANGO MAKEMIGRATIONS COMPLETED."
-else
-    echo ">>> [${RED}ERROR${NC}]: DJANGO MAKEMIGRATIONS FAILED."
-fi
+    echo ">>> [${BLUE}INFO${NC}]: DJANGO MAKEMIGRATIONS."
+    python ./api/manage.py makemigrations
+    if [ $? -eq 0 ]
+    then
+        echo ">>> [${GREEN}SUCCESS${NC}]: DJANGO MAKEMIGRATIONS COMPLETED."
+    else
+        echo ">>> [${RED}ERROR${NC}]: DJANGO MAKEMIGRATIONS FAILED."
+    fi
 
-echo ">>> [${BLUE}INFO${NC}]: DJANGO MIGRATE."
-python ./api/manage.py migrate
-if [ $? -eq 0 ]
-then
-    echo ">>> [${GREEN}SUCCESS${NC}]: DJANGO MIGRATE COMPLETED."
-else
-    echo ">>> [${RED}ERROR${NC}]: DJANGO MIGRATE FAILED."
-fi
+    echo ">>> [${BLUE}INFO${NC}]: DJANGO MIGRATE."
+    python ./api/manage.py migrate
+    if [ $? -eq 0 ]
+    then
+        echo ">>> [${GREEN}SUCCESS${NC}]: DJANGO MIGRATE COMPLETED."
+    else
+        echo ">>> [${RED}ERROR${NC}]: DJANGO MIGRATE FAILED."
+    fi
 
-echo ">>> [${BLUE}INFO${NC}]: CREATING DJANGO SUPERUSER."
-python ./api/manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
-if [ $? -eq 0 ]
-then
-    echo ">>> [${GREEN}SUCCESS${NC}]: DJANGO SUPERUSER CREATED."
-else
-    echo ">>> [${RED}ERROR${NC}]: DJANGO CREATING SUPERUSER FAILED."
-fi
+    echo ">>> [${BLUE}INFO${NC}]: CREATING DJANGO SUPERUSER."
+    python ./api/manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
+    if [ $? -eq 0 ]
+    then
+        echo ">>> [${GREEN}SUCCESS${NC}]: DJANGO SUPERUSER CREATED."
+    else
+        echo ">>> [${RED}ERROR${NC}]: DJANGO CREATING SUPERUSER FAILED."
+    fi
 
-echo ">>> [${BLUE}INFO${NC}]: LOAD FIXTURES INTO DATABASE."
-python ./api/manage.py loaddata ./api/blog/fixtures/db_fixtures.json
-if [ $? -eq 0 ]
-then
-    echo ">>> [${GREEN}SUCCESS${NC}]: FIXTURES LOADED."
-else
-    echo ">>> [${RED}ERROR${NC}]: LOADING FIXTURES FAILED."
-fi
+    echo ">>> [${BLUE}INFO${NC}]: LOAD FIXTURES INTO DATABASE."
+    python ./api/manage.py loaddata ./api/blog/fixtures/db_fixtures.json
+    if [ $? -eq 0 ]
+    then
+        echo ">>> [${GREEN}SUCCESS${NC}]: FIXTURES LOADED."
+    else
+        echo ">>> [${RED}ERROR${NC}]: LOADING FIXTURES FAILED."
+    fi
 
-echo ">>> [${BLUE}INFO${NC}]: EXECUTING $@."
-exec "$@"
+    echo ">>> [${BLUE}INFO${NC}]: EXECUTING $@."
+    exec "$@"
+fi
